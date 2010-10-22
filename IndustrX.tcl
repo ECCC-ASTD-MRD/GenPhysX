72ff71b30846d76eb1a763e021dfe0190e638baa
Lucie Boucher <fake.email@ec.gc.ca>
2010-10-22 18:31:00 +0000

    Addition of ifelse in main process to be able to skip a certain number of NTS Sheets that were already computed (useful if IndustrX crashes while computing a big province, to avoid loosing time).

--- IndustrX.tcl
+++ IndustrX.tcl
@@ -53,7 +53,7 @@ proc UrbanX::Priorities2SMOKE {indexCouverture } {
 	#lecture des fichiers créés précédemment lors des procs SandwichCanVec et PopDens2BuiltupCanVec
    gdalband read RSANDWICH [gdalfile open FSANDWICH read $GenX::Param(OutFile)_sandwich_$indexCouverture.tif]
    gdalband read RPOPDENSCUT [gdalfile open FPOPDENSCUT read $GenX::Param(OutFile)_popdens-builtup_$indexCouverture.tif]
-   gdalband read RLCC2000VSMOKE [gdalfile open FLCC2000VSMOKE read $GenX::Param(OutFile)_LCC2000VSMOKE_$indexCouverture.tif]
+#   gdalband read RLCC2000VSMOKE [gdalfile open FLCC2000VSMOKE read $GenX::Param(OutFile)_LCC2000VSMOKE_$indexCouverture.tif]
 
 	#passage des valeurs de priorités (sandwich) aux valeurs smoke dans RSMOKE
    vector create LUT
@@ -67,7 +67,7 @@ proc UrbanX::Priorities2SMOKE {indexCouverture } {
    vexpr RSMOKE ifelse(RPOPDENSCUT!=0,RPOPDENSCUT,RSMOKE)
 
 	#modification pour inclure la végétation LCC2000V
-   vexpr RSMOKE ifelse(RSMOKE==0 || RSMOKE==200,RLCC2000VSMOKE,RSMOKE)
+#   vexpr RSMOKE ifelse(RSMOKE==0 || RSMOKE==200,RLCC2000VSMOKE,RSMOKE)
 
 	#écriture du fichier de sortie
    file delete -force $GenX::Param(OutFile)_SMOKE_$indexCouverture.tif
@@ -76,8 +76,8 @@ proc UrbanX::Priorities2SMOKE {indexCouverture } {
 
    GenX::Log INFO "The file $GenX::Param(OutFile)_SMOKE_$indexCouverture.tif was generated"
 
-   gdalfile close FILEOUT FSANDWICH FPOPDENSCUT ;#FVEGESMOKE
-   gdalband free RSMOKE RSANDWICH RPOPDENSCUT ;#RVEGESMOKE
+   gdalfile close FILEOUT FSANDWICH FPOPDENSCUT ;#FLCC2000VSMOKE
+   gdalband free RSMOKE RSANDWICH RPOPDENSCUT ;#RLCC2000VSMOKE
 
    GenX::Log INFO "Fin de la proc Priorities2SMOKE"
 }
@@ -218,82 +218,91 @@ proc IndustrX::Process { Coverage } {
 	#----- Process for each NTS Sheets that were previously selected
 	foreach feuillet $UrbanX::Param(NTSSheets) {
 
-		#calcul du temps de traitement d'un feuillet NTS
-		set t_feuillet [clock seconds]
+		#si le traitement d'une province a déjà été partiellement effectué, écrire l'index de feuillet où reprendre le traitement
+		if {$i < 1} {
+			GenX::Log INFO "Feuillet $i sur $nbrfeuillets"
+			GenX::Log INFO "Feuillet déjà traité"
+			puts "__________________________________________________________________________________________________________________________"
+			incr i
 
-		GenX::Log INFO "Feuillet $i sur $nbrfeuillets"
-
-		GenX::Log INFO "Traitement de la tuile NTS ayant le numéro de feuillet $feuillet"
-
-		#----- Finds the extents of the zone (NTS Sheet) to be process
-		UrbanX::NTSExtent $feuillet
-		GenX::Log INFO "La latitude de la tuile va de $UrbanX::Param(Lat0) à $UrbanX::Param(Lat1)"
-		GenX::Log INFO "La longitude de la tuile va de $UrbanX::Param(Lon0) à $UrbanX::Param(Lon1)"
-		GenX::Log INFO "Les coordonnées UTM de la tuile sur l'axe X  vont de $UrbanX::Param(x0) à $UrbanX::Param(x1)"
-		GenX::Log INFO "Les coordonnées UTM de la tuile sur l'axe Y  vont de $UrbanX::Param(y0) à $UrbanX::Param(y1)"
-
-		#----- Finds CanVec files, rasterize and flattens all CanVec layers, applies buffer on some elements
-		set UrbanX::Param(t_Sandwich) 0
-		if { ![file exists $GenX::Param(OutFile)_sandwich_$feuillet.tif] } {
-			UrbanX::Sandwich $feuillet
 		} else {
-			GenX::Log INFO "Le fichier $GenX::Param(OutFile)_sandwich_$feuillet.tif existe déjà."
-		}
+			#calcul du temps de traitement d'un feuillet NTS
+			set t_feuillet [clock seconds]
 
-		#----------TO MODIFY FOR CANVEC LAYERS
-# 		#----- Creates the fields and building vicinity output using spatial buffers
-# 		UrbanX::ChampsBuffers $m
-		#----------END OF : TO MODIFY FOR CANVEC LAYERS
+			GenX::Log INFO "Feuillet $i sur $nbrfeuillets"
 
-# 		#LCC2000V Vegetation
-# 		if { ![file exists $GenX::Param(OutFile)_LCC2000V_$feuillet.tif] || ![file exists $GenX::Param(OutFile)_LCC2000VSMOKE_$feuillet.tif] } {
-# 			UrbanX::LCC2000V $feuillet
-# 		} else {
-# 			GenX::Log INFO "Les fichiers $GenX::Param(OutFile)_LCC2000V_$feuillet.tif et $GenX::Param(OutFile)_LCC2000VSMOKE_$feuillet.tif existent déjà."
-# 		}
+			GenX::Log INFO "Traitement de la tuile NTS ayant le numéro de feuillet $feuillet"
 
-		#----- Calculates the population density and split the residential areas according to population density thresholds
-		set UrbanX::Param(t_PopDens2Builtup) 0
-		if { ![file exists $GenX::Param(OutFile)_popdens_$feuillet.tif] || ![file exists $GenX::Param(OutFile)_popdens-builtup_$feuillet.tif] } {
-			UrbanX::PopDens2Builtup $feuillet
-		} else {
-			GenX::Log INFO "Les fichiers $GenX::Param(OutFile)_popdens_$feuillet.tif et $GenX::Param(OutFile)_popdens-builtup_$feuillet.tif existent déjà."
-		}
+			#----- Finds the extents of the zone (NTS Sheet) to be process
+			UrbanX::NTSExtent $feuillet
+			GenX::Log INFO "La latitude de la tuile va de $UrbanX::Param(Lat0) à $UrbanX::Param(Lat1)"
+			GenX::Log INFO "La longitude de la tuile va de $UrbanX::Param(Lon0) à $UrbanX::Param(Lon1)"
+			GenX::Log INFO "Les coordonnées UTM de la tuile sur l'axe X  vont de $UrbanX::Param(x0) à $UrbanX::Param(x1)"
+			GenX::Log INFO "Les coordonnées UTM de la tuile sur l'axe Y  vont de $UrbanX::Param(y0) à $UrbanX::Param(y1)"
 
-		#----- Applies LUT to all processing results to generate SMOKE classes and sets the values in the DA shapefile
-		set Param(t_Priorities2SMOKE) 0
-		set Param(t_SMOKE2DA) 0
-		if { ![file exists $GenX::Param(OutFile)_SMOKE_$feuillet.tif] } {
-			#----- Applies LUT to all processing results to generate SMOKE classes
-			UrbanX::Priorities2SMOKE  $feuillet
-			#----- Counts the SMOKE values and write the results in the dissemination area shapefile
-			UrbanX::SMOKE2DA $feuillet
-		} else {
-			GenX::Log INFO "Le fichier $GenX::Param(OutFile)_SMOKE_$feuillet.tif existe déjà."
-		}
+			#----- Finds CanVec files, rasterize and flattens all CanVec layers, applies buffer on some elements
+			set UrbanX::Param(t_Sandwich) 0
+			if { ![file exists $GenX::Param(OutFile)_sandwich_$feuillet.tif] } {
+				UrbanX::Sandwich $feuillet
+			} else {
+				GenX::Log INFO "Le fichier $GenX::Param(OutFile)_sandwich_$feuillet.tif existe déjà."
+			}
 
-		#suppression des produits intermédiaires
-		file delete -force $GenX::Param(OutFile)_sandwich_$feuillet.tif
-		GenX::Log INFO "The file $GenX::Param(OutFile)_sandwich_$feuillet.tif was deleted"
-		file delete -force $GenX::Param(OutFile)_popdens_$feuillet.tif
-		GenX::Log INFO "The file $GenX::Param(OutFile)_popdens_$feuillet.tif was deleted"
-		file delete -force $GenX::Param(OutFile)_popdens-builtup_$feuillet.tif
-		GenX::Log INFO "The file $GenX::Param(OutFile)_popdens-builtup_$feuillet.tif was deleted"
-		file delete -force $GenX::Param(OutFile)_EOSDVegetation_$feuillet.tif
-		GenX::Log INFO "The file $GenX::Param(OutFile)_EOSDVegetation_$feuillet.tif was generated"
-		file delete -force $GenX::Param(OutFile)_EOSDSMOKE_$feuillet.tif
-		GenX::Log INFO "The file $GenX::Param(OutFile)_EOSDSMOKE_$feuillet.tif was generated"
-		file delete -force $GenX::Param(OutFile)_SMOKE_$feuillet.tif
-		GenX::Log INFO "The file $GenX::Param(OutFile)_SMOKE_$feuillet.tif was deleted"
-# 
-		#affichage du temps de traitement du feuillet
-		puts "Feuillet $feuillet traité en [expr [clock seconds]-$t_feuillet] secondes"
+			#----------TO MODIFY FOR CANVEC LAYERS
+	# 		#----- Creates the fields and building vicinity output using spatial buffers
+	# 		UrbanX::ChampsBuffers $m
+			#----------END OF : TO MODIFY FOR CANVEC LAYERS
 
-		#préparation à la nouvelle incrémentation
-		puts "__________________________________________________________________________________________________________________________"
-		incr i
+	# 		#LCC2000V Vegetation
+	# 		if { ![file exists $GenX::Param(OutFile)_LCC2000V_$feuillet.tif] || ![file exists $GenX::Param(OutFile)_LCC2000VSMOKE_$feuillet.tif] } {
+	# 			UrbanX::LCC2000V $feuillet
+	# 		} else {
+	# 			GenX::Log INFO "Les fichiers $GenX::Param(OutFile)_LCC2000V_$feuillet.tif et $GenX::Param(OutFile)_LCC2000VSMOKE_$feuillet.tif existent déjà."
+	# 		}
+
+			#----- Calculates the population density and split the residential areas according to population density thresholds
+			set UrbanX::Param(t_PopDens2Builtup) 0
+			if { ![file exists $GenX::Param(OutFile)_popdens_$feuillet.tif] || ![file exists $GenX::Param(OutFile)_popdens-builtup_$feuillet.tif] } {
+				UrbanX::PopDens2Builtup $feuillet
+			} else {
+				GenX::Log INFO "Les fichiers $GenX::Param(OutFile)_popdens_$feuillet.tif et $GenX::Param(OutFile)_popdens-builtup_$feuillet.tif existent déjà."
+			}
+
+			#----- Applies LUT to all processing results to generate SMOKE classes and sets the values in the DA shapefile
+			set Param(t_Priorities2SMOKE) 0
+			set Param(t_SMOKE2DA) 0
+			if { ![file exists $GenX::Param(OutFile)_SMOKE_$feuillet.tif] } {
+				#----- Applies LUT to all processing results to generate SMOKE classes
+				UrbanX::Priorities2SMOKE  $feuillet
+				#----- Counts the SMOKE values and write the results in the dissemination area shapefile
+				UrbanX::SMOKE2DA $feuillet
+			} else {
+				GenX::Log INFO "Le fichier $GenX::Param(OutFile)_SMOKE_$feuillet.tif existe déjà."
+			}
+
+			#suppression des produits intermédiaires
+			file delete -force $GenX::Param(OutFile)_sandwich_$feuillet.tif
+			GenX::Log INFO "The file $GenX::Param(OutFile)_sandwich_$feuillet.tif was deleted"
+			file delete -force $GenX::Param(OutFile)_popdens_$feuillet.tif
+			GenX::Log INFO "The file $GenX::Param(OutFile)_popdens_$feuillet.tif was deleted"
+			file delete -force $GenX::Param(OutFile)_popdens-builtup_$feuillet.tif
+			GenX::Log INFO "The file $GenX::Param(OutFile)_popdens-builtup_$feuillet.tif was deleted"
+			file delete -force $GenX::Param(OutFile)_EOSDVegetation_$feuillet.tif
+			GenX::Log INFO "The file $GenX::Param(OutFile)_EOSDVegetation_$feuillet.tif was generated"
+			file delete -force $GenX::Param(OutFile)_EOSDSMOKE_$feuillet.tif
+			GenX::Log INFO "The file $GenX::Param(OutFile)_EOSDSMOKE_$feuillet.tif was generated"
+			file delete -force $GenX::Param(OutFile)_SMOKE_$feuillet.tif
+			GenX::Log INFO "The file $GenX::Param(OutFile)_SMOKE_$feuillet.tif was deleted"
+	# 
+			#affichage du temps de traitement du feuillet
+			puts "Feuillet $feuillet traité en [expr [clock seconds]-$t_feuillet] secondes"
+
+			#préparation à la nouvelle incrémentation
+			puts "__________________________________________________________________________________________________________________________"
+			incr i
+		} ;#fin du traitement du feuillet (boucle else)
 	} ;# fin du foreach feuillet
-# 
+ 
 	#fermeture du fichier de polygones de DA à modifier avec les valeurs SMOKE
 	#ogrfile close SHAPEDSMOKE  ;#ne pas le mettre, vu un bogue avec le mode append
 	ogrlayer free VDASMOKE  
