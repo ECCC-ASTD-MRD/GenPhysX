705992dc6eb321b4b0f8ae3f8e950261109a3e61
Jean-Philippe Gauthier <Jean-Philippe.Gauthier@canada.ca>
2019-03-06 15:22:20 +0000

    Use CI_DATA//in and out for testing.

--- test/CI.sh
+++ test/CI.sh
@@ -11,12 +11,10 @@ cd ${CI_PROJECT_DIR}
 
 export SPI_LIB=${SSM_DEV}/workspace/libSPI_${SPI_VERSION}${SSM_COMP}_${ORDENV_PLAT}
 export SPI_PATH=${SSM_DEV}/workspace/SPI_${SPI_VERSION}_all
-export CI_GENPYSX_IN=/home/nil000/links/eccc-ppp1/storage/SPI/DataIn
-export CI_GENPYSX_OUT=`mktemp -d`
+export CI_DATA_IN=${CI_DATA}/GenPhysX/in
+export CI_DATA_OUT=${CI_DATA}/GenPhysX/out/${CI_BUILD_REF}
 
 #----- Launch tests
-mkdir -p $CI_GENPYSX_OUT
-echo "Path  : ${CI_GENPYSX_OUT}" > ${CI_PROJECT_DIR}/CI
-echo "Log   : GenPhysX-${CI_BUILD_REF}.log" >> ${CI_PROJECT_DIR}/CI
-${CI_PROJECT_DIR}/bin/GenPhysX -target GDPS_5.1 -gridfile ${CI_GENPYSX_IN}/GDPS_5.1.fst -result ${CI_GENPYSX_OUT}/GDPS_5.1 > GenPhysX-${CI_BUILD_REF}.log
-echo "Status: $?" >> ${CI_PROJECT_DIR}/CI
+mkdir -p $CI_DATA_OUT
+${CI_PROJECT_DIR}/bin/GenPhysX -target GDPS_5.1 -gridfile ${CI_DATA_IN}/GDPS_5.1.fst -result ${CI_DATA_OUT}/GDPS_5.1 > ${CI_PROJECT_DIR}/CI.log
+echo "Status: $?" >> ${CI_PROJECT_DIR}/CI.log
